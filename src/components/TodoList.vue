1382eb1cd2ab97d57fe151fe73037ef4a3635066
zarembovskiy.v <vzarembovskiy@marathonbet.ru>
2017-08-18 18:34:31 +0300

    Initial commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    props: ['todos']
+  }
+</script>
+
+<style>
+</style>
